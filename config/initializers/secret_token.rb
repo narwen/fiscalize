e8cbbeed00174e9195c54a77eeb22856555ab303
Gabriel Reis and Thais Camilo <narwen+greis+narwen@gmail.com>
2011-04-16 23:31:22 -0400

    initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiscalize::Application.config.secret_token = '9a89df763661b86f6d506852d31f7bbcd11764f5039297356d440a8f5d72514b61b901fb5a0583dcf40bd5b56ae60da7809708f009667b3fe85e64d9af0217e1'
